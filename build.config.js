f2902ce01815689c12977ccf4b43afa6f6f8e2f7
Aaron <aaronchristopherw@gmail.com>
2014-10-11 15:26:16 -0700

    Added build to compile task. Fixed bug where ui-utils bower dependency was incorrect. Added additional copyright line to LICENSE.

--- build.config.js
+++ build.config.js
@@ -66,11 +66,11 @@ module.exports = {
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/ui-utils.js'
     ],
     css: [
     ],
     assets: [
     ]
-  },
+  }
 };
